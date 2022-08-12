d21f378036b2b899fe9646a0e4bc23d26ac6a2cd
njulio6 <njulio6@gmail.com>
2022-08-11 21:14:52 -0400

    newcopy

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+# s3 
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "kojitech.terraform.101.state.gfdhe" # create variable, 
+}
+
+resource "aws_s3_bucket_acl" "state_bucket_acl" {
+  bucket = aws_s3_bucket.state_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
+  bucket = aws_s3_bucket.state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "terraform-state-lock"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  
+}
